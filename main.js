5705bf617037538fe3faab6cc48e07e782b85986
Lasha <lashagoglidzelasha@gmail.com>
2023-02-15 21:49:50 +0400

    Final Version

---
+++ main.js
@@ -0,0 +1,7 @@
+let transperentDiv = document.querySelector(".transperentDiv");
+let button = document.querySelector("button");
+
+button.addEventListener("click",function(){
+    transperentDiv.classList.toggle("btn");
+    
+})
